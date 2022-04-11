a129ca48c49bfedd12147bc639408272db72a293
Hoshhh <nyxious123@live.com>
2022-04-11 13:51:03 -0400

    Flipping Bits

---
+++ 6.js
@@ -0,0 +1,9 @@
+bit = 00000000000000000000000000000001
+
+function flippingBits(n) {
+    //~ is the bitwise NOT operator and is used to invert bits (0 -> 1) 
+    //>>> returns it as an unsigned integer 
+    console.log(~n >> 0)
+}
+
+flippingBits(bit)
